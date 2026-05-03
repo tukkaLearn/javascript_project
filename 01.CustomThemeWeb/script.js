6641b3897b2cb39aeaab61376974ff95f6ea500b
itzDm <bitdev20@gmail.com>
2026-05-03 20:09:58 +0530

    project readme add

--- 01.CustomThemeWeb/script.js
+++ 01.CustomThemeWeb/script.js
@@ -1,5 +1,1 @@
-document.querySelector("#btn").addEventListener("click", () => {
-  document.body.style.backgroundColor =
-    document.querySelector("#bgColor").value;
-  document.body.style.color = document.querySelector("#textColor").value;
-});
+// --------------Todo----------------
