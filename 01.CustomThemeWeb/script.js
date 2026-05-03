349488bbfc1a62e1de537a49482aa27f392a7a46
itzDm <bitdev20@gmail.com>
2026-05-03 19:19:13 +0530

    project 01 complete

--- 01.CustomThemeWeb/script.js
+++ 01.CustomThemeWeb/script.js
@@ -1,1 +1,5 @@
-// --------------Todo----------------
+document.querySelector("#btn").addEventListener("click", () => {
+  document.body.style.backgroundColor =
+    document.querySelector("#bgColor").value;
+  document.body.style.color = document.querySelector("#textColor").value;
+});
