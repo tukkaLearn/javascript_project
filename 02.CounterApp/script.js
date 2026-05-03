b8dbb3d4e9607f879ef13c9b775bda0e795c9d21
itzDm <bitdev20@gmail.com>
2026-05-03 22:26:04 +0530

    project 02 complete

--- 02.CounterApp/script.js
+++ 02.CounterApp/script.js
@@ -1,1 +1,32 @@
-// --------------Todo----------------
+const increase = document.querySelector("#increase");
+const decrease = document.querySelector("#decrease");
+const reset = document.querySelector("#reset");
+const counter = document.querySelector("#counter");
+
+let count = 0;
+
+function updateColor() {
+  if (count > 0) {
+    counter.style.color = "green";
+  } else if (count < 0) {
+    counter.style.color = "red";
+  } else {
+    counter.style.color = "black";
+  }
+}
+
+increase.addEventListener("click", () => {
+  count++;
+  counter.textContent = count;
+  updateColor();
+});
+decrease.addEventListener("click", () => {
+  count--;
+  counter.textContent = count;
+  updateColor();
+});
+reset.addEventListener("click", () => {
+  count = 0;
+  counter.textContent = count;
+  updateColor();
+});
