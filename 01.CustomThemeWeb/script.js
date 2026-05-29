324f413091194e3ffd07b792c549ad1450a860d8
itzDm <bitdev20@gmail.com>
2026-05-29 22:50:24 +0530

    Tukka Theme Studio completed

--- 01.CustomThemeWeb/script.js
+++ 01.CustomThemeWeb/script.js
@@ -1,5 +1,16 @@
-document.querySelector("#btn").addEventListener("click", () => {
-  document.body.style.backgroundColor =
-    document.querySelector("#bgColor").value;
-  document.body.style.color = document.querySelector("#textColor").value;
+const textColor = document.querySelector("#textColor");
+const bgColor = document.querySelector("#bgColor");
+const resetBtn = document.querySelector("#resetBtn");
+const applyBtn = document.querySelector("#applyBtn");
+
+applyBtn.addEventListener("click", () => {
+  document.body.style.backgroundColor = bgColor.value;
+  document.body.style.color = textColor.value;
+});
+
+resetBtn.addEventListener("click", () => {
+  document.body.style.backgroundColor = "#0f172a";
+  bgColor.value = "#0f172a";
+  document.body.style.color = "#ffffff";
+  textColor.value = "#ffffff";
 });
