516a6cd26ab7b0f881df1e737944da22dcc28ad1
itzDm <bitdev20@gmail.com>
2026-06-07 11:31:54 +0530

    counter  project js modify

--- 02.CounterApp/script.js
+++ 02.CounterApp/script.js
@@ -1,1 +1,12 @@
 // --------------Todo----------------
+// - Select Counter Element by ID 'counter'
+// - Select Increment Button by ID 'increase'
+// - Select Decrement Button by ID 'decrease'
+// - Select Reset Button by ID 'reset'
+// - Increment Counter by 1
+// - Decrement Counter by 1
+// - Reset Counter to 0
+// - Update Color of Counter based on Counter Value
+// - If positive Counter Value Update Counter Color to Green
+// - If negative Counter Value Update Counter Color to Red
+// - If Counter Value is 0 Update Counter Color to white
