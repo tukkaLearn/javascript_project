06d9eeff302496a9e0e9a931aa832e7fb086dc75
itzDm <bitdev20@gmail.com>
2026-06-07 11:20:14 +0530

    theme project js modify

--- 01.CustomThemeWeb/script.js
+++ 01.CustomThemeWeb/script.js
@@ -1,1 +1,6 @@
 // --------------Todo----------------
+// - Select textColor using textColor id
+// - Select bgColor using bgColor id
+// - Get value of textColor and bgColor
+// - Select applyBtn button  using applyBtn id to apply textColor and bgColor
+// - Select reset button  using resetBtn id to reset textColor and bgColor
