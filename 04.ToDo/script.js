3cf7e9dc689075a883478006576764106f270baf
itzDm <bitdev20@gmail.com>
2026-06-07 11:41:35 +0530

    todo project js modify

--- 04.ToDo/script.js
+++ 04.ToDo/script.js
@@ -1,1 +1,13 @@
-// Todo
+// -----------Todo--------------------------
+//  FakeData Base Like Setup const todos = [];
+//  Set  let currentFilter = "all"; // 'all', 'pending', 'completed'
+//  Set let currentSort = "newest"; // 'newest', 'oldest'
+
+//  Select taskInput  Element using 'taskInput' id
+//  Select btnAdd Element using 'btnAdd' id
+//  Select todoList Element using 'todo-list' class first element
+//  Select completedBtn Element using 'completed' id
+//  Select pendingBtn Element using 'pending' id
+//  Select allBtn Element using 'all' id
+//  Select sortBtn Element using 'sort' id
+//  Select taskCount Element using 'taskCount' id
