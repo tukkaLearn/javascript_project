0ff3a71a2b38113a3ce6c939b1f84b322220381d
itzDm <bitdev20@gmail.com>
2026-06-14 05:52:56 +0530

    tukka image slder  auto layout  comp

---
+++ ImageSliderAuto/script.js
@@ -0,0 +1,27 @@
+const imagesData = [
+  {
+    link: "https://st.depositphotos.com/36924814/56285/i/450/depositphotos_562858084-stock-photo-dot-blue-wave-light-screen.jpg",
+    tag: "Tukka Projects",
+    title: "Wave of Blue Light",
+    description: "Stylish layouts using CSS ",
+  },
+  {
+    link: "https://img.freepik.com/premium-photo/background-concept-with-abstract-data-design_327072-25004.jpg",
+    tag: "Tukka Learn",
+    title: "Sky Blue Background",
+    description: "Sky blue background with smooth transitions",
+  },
+  {
+    link: "https://img.freepik.com/premium-photo/background-concept-with-abstract-data-design_327072-24787.jpg",
+    tag: "Tukka Projects",
+    title: "Creative Modern UI ",
+    description: "Stylish layouts with smooth transitions ",
+  },
+];
+
+// ---------------Todo--------------
+
+// Select slider container Element using 'slider-container' id
+// Select nextBtn Element using 'next-btn' class first element
+// Select prevBtn Element using 'prev-btn' class first element
+// Create slide in slider container,get things from HTML File Where you have to modify
