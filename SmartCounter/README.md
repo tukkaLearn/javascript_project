22538229556b9ee5d6190128e0c967cc0ba65a0c
itzDm <bitdev20@gmail.com>
2026-06-14 11:19:04 +0530

    tukka smart counter layout  COMP

---
+++ SmartCounter/README.md
@@ -0,0 +1,23 @@
+# Title: Smart Counter
+
+## Goal: DOM + Keyboard Events +Theme Toggle
+
+### Features
+
+- Keyboard Events
+- Theme Toggle
+- increment ,decrement and reset
+- 'R' for reset
+- '+' for increment
+- '-' for decrement
+- Local Storage
+
+### Concepts Covered
+
+- event handling
+- keyboard events
+- Local Storage
+
+---
+
+<h2 align="center">|| राम नाम सत्य है ||</h2>
---
+++ SmartCounter/script.js
@@ -0,0 +1,21 @@
+// ELEMENTS
+
+const counterElement = document.getElementById("counter");
+
+const incrementBtn = document.getElementById("incrementBtn");
+
+const decrementBtn = document.getElementById("decrementBtn");
+
+const resetBtn = document.getElementById("resetBtn");
+
+const themeBtn = document.getElementById("themeBtn");
+
+// ---------ToDo------------
+
+// COUNTER VALUE FROM LOCAL STORAGE and THEME
+// INCREMENT FUNCTION
+// DECREMENT FUNCTION
+// RESET FUNCTION
+// BUTTON EVENTS
+// THEME TOGGLE FUNCTION
+// KEYBOARD EVENTS
